299e6f3948a9e8123b3e14ef2813fa54ffda162e
Samantha Siqueira <samantharsiqueira@gmail.com>
2024-07-19 16:49:14 +0100

    feat: login router

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import productRouter from './routes/productRouter';
+import loginRouter from './routes/loginRouter';
 
 const app = express();
 
 app.use(express.json());
 app.use(productRouter);
+app.use(loginRouter);
 
 export default app;
---
+++ src/routes/loginRouter.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+import loginController from '../controllers/loginController';
+import { ROUTE } from '../utils/index';
+
+const loginRouter = express.Router();
+
+loginRouter.post(ROUTE.LOGIN, loginController.login);
